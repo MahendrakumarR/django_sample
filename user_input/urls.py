b544a744cc571ff233abec8eded72bc3564fa60b
MahendraN <mahendraramalingam8056@gmail.com>
2024-03-24 13:00:03 +0530

    update

--- user_input/urls.py
+++ user_input/urls.py
@@ -2,7 +2,7 @@ from django .urls import path
 from . import views
 
 urlpatterns=[
-    path("",views.user_input, name="user_input/"),
-    path("user_output/",views.user_output, name="user_output")
+    path("",views.user_input, name="user_input"),
+    path("user_output",views.user_output, name="user_output")
     
 ]
--- user_input/views.py
+++ user_input/views.py
@@ -3,9 +3,12 @@ from django.http import HttpResponse
 
 
 def user_input(request):
-    return render(request, 'user_input.html')
+    return render(request, 'user.html')
 def user_output(request):
-    return render(request, 'user_output.html')
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    phone = request.POST.get('phone')
+    return render(request, 'user_output.html', {'name':name, 'email':email, 'phone':phone})
 
     
 
