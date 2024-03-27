46f777146907d356afa539f31fea83407bfcaa1e
MahendraN <mahendraramalingam8056@gmail.com>
2024-03-27 14:15:44 +0530

    update

--- bootstrap_design/urls.py
+++ bootstrap_design/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.boot, name='bootstrap_design'),
+    path('', views.boot, name='boot'),
     path('boot_out', views.boot_out, name='boot_out')
     
 ]
+   
--- bootstrap_design/views.py
+++ bootstrap_design/views.py
@@ -8,5 +8,5 @@ def boot_out(request):
     name = request.POST.get('name')
     age = request.POST.get('age')
     phone = request.POST.get('phone')
-    return render(request, 'boot_out.html', {'name':name, 'Age':age, 'phone':phone})
+    return render(request, 'boot_out.html', {'name':name, 'age':age, 'phone':phone})
 # Create your views here.
