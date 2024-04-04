cb34bf748f99578762df93ff461d47975584112c
MahendraN <mahendraramalingam8056@gmail.com>
2024-04-04 05:37:05 +0530

    update

--- sqlite_proj/models.py
+++ sqlite_proj/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
+class register(models.Model):
+    Name = models.CharField(max_length=20, default="")
+    Age = models.IntegerField(max_length=20, default="")
+    Address = models.CharField(max_length=50, default="")
+    Phone = models.IntegerField(max_length=20, default="")
+    
+
+
+
+
 # Create your models here.
