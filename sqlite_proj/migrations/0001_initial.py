b7ae62298923eba095f6d5a0ccd149c28ffab986
MahendraN <mahendraramalingam8056@gmail.com>
2024-04-05 20:22:46 +0530

    update

--- sqlite_proj/migrations/0001_initial.py
+++ sqlite_proj/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Datas',
+            name='info',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('Name', models.CharField(default='', max_length=20)),
--- sqlite_proj/models.py
+++ sqlite_proj/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
-
-class Datas(models.Model):    # here create a new class for storing values to database
+class MyInfo(models.Model):
     Name = models.CharField(max_length=20, default="")
-    Age = models.IntegerField(max_length=20, default="")
+    Age = models.IntegerField(default="")
     Address = models.CharField(max_length=50, default="")
-    Phone = models.IntegerField(max_length=20, default="")
-    
+    Phone = models.IntegerField(default="")
 
-
+    class Meta:
+        db_table = 'custom_table_name'  # Specify a custom table name here
 
 
 # Create your models here.
