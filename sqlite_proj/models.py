2a0949819320ff1aa918b4522b9b60fb00d80855
MahendraN <mahendraramalingam8056@gmail.com>
2024-04-06 17:09:56 +0530

    update

--- sqlite_proj/models.py
+++ sqlite_proj/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class MyInfo(models.Model):
-    Name = models.CharField(max_length=20, default="")
-    Age = models.IntegerField(default="")
-    Address = models.CharField(max_length=50, default="")
-    Phone = models.IntegerField(default="")
+    name = models.CharField(max_length=20, default="")
+    age = models.IntegerField(default="")
+    address = models.CharField(max_length=50, default="")
+    phone = models.IntegerField(default="")
 
     class Meta:
         db_table = 'custom_table_name'  # Specify a custom table name here
--- sqlite_proj/views.py
+++ sqlite_proj/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from.models import MyInfo            # import 'models.py' for store data into database
 
 
 def data (request):
+    if request.method =='POST':
+        Name = request.POST['name']    # here get data from html file  # then Name is varible to sotre value of 'name' from html file
+        Age = request.POST['age']      # here get data from html file 
+        Address = request.POST['address']   # here get data from html file 
+        Phone = request.POST['phone']       # here get data from html file 
+
+        obj = MyInfo()                # 'obj' is object of 'MyInfo' class from 'models.py' file for pass data to sqlite database
+        obj.name = Name           # here 'obj.name' is call the 'models.py' class variable 'name'
+        obj.age = Age             # here 'obj.age' is call the 'models.py' class variable 'age'
+        obj.address = Address     # here 'obj.address' is call the 'models.py' class variable 'address'
+        obj.phone = Phone         # here 'obj.phone' is call the 'models.py' class variable 'phone'
+        obj.save()          # here use 'save' for save all data into database
     return render(request, "sql_front.html")
 
 # Create your views here.
