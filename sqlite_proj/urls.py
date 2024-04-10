9acf8aa5061e742fab3324d16ba96f1f8b4f497c
MahendraN <mahendraramalingam8056@gmail.com>
2024-04-10 05:51:53 +0530

    update

--- sqlite_proj/urls.py
+++ sqlite_proj/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.data, name='sqlite_proj'),
     path('adata', views.adata, name='sqlite_adata'), # here 'adata' mean add data
-    path('udata<int:id>', views.udata, name='sqlite_udata'), # here 'udata' mean update data
+    path('udata/<int:id>', views.udata, name='sqlite_udata'), # here 'udata' mean update data
      # here 'ddata' mean delete data
 ]
