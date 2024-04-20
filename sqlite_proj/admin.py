9bbd4154ae093ad34f719025ab65140d4a7ac1b7
MahendraN <mahendraramalingam8056@gmail.com>
2024-04-20 20:33:33 +0530

    Update

--- sqlite_proj/admin.py
+++ sqlite_proj/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from.models import MyInfo    # Here import the class name of the models used "Datas"
 
-admin.site.register(MyInfo)   # Here register the models to store the values into database
+admin.site.register(MyInfo)   # Here Register the models to store the values into database
 # Register your models here.
